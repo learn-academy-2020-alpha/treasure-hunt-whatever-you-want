0df9d7b46946c402cbb5c20550950c3300fd428c
haizhou85 <60331375+haizhou85@users.noreply.github.com>
2020-02-20 16:47:21 -0800

    added working bomb and treasure

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,16 +6,35 @@ constructor (props){
     super(props)
     this.state = {
         board: ["?","?","?","?","?","?","?","?","?"],
-        tree: "🌴"
+        tree: "🌴",
+        treasure: null,
+        bomb: null,
+        treasureIcon: "🏆",
+        bombIcon: "💣"
+
     }
 }
     handleLocation = (index) => {
-
+        //if clicked index === bombLocation retun bomblocation
+        if (index === this.state.treasure){
+            this.state.board[index] = this.state.treasureIcon
+            this.setState({board: this.state.board})
+        }else if (index === this.state.bomb){
+            this.state.board[index] = this.state.bombIcon
+            this.setState({board: this.state.board})
+        }else
         this.state.board[index] = this.state.tree
-        this.setState({board: this.state.board })
-        
+        this.setState({board: this.state.board})
     }
-
+    componentDidMount = () => {
+        let {board, tree, bomb, treasure} = this.state
+        const treasureLocation =  (Math.floor(Math.random() * board.length))
+        console.log(treasureLocation);
+         this.setState({treasure: treasureLocation})
+        const bombLocation = (Math.floor(Math.random() * board.length))
+        console.log(bombLocation);
+            this.setState({bomb: bombLocation})
+     }
   render() {
       let square = this.state.board.map((value, index) => {
           return(
